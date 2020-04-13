f036e387d663b2df7732e274d6e08dcbf77582d7
Pedro <pvabreu7@gmail.com>
2020-04-13 14:52:51 -0300

    Updated table grid

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import dash_core_components as dcc  # São componentes interativos gerados com js, html e css através do reactjs
 import dash_html_components as html # Possui um componente para cada tag html
 import plotly.express as px
@@ -16,18 +17,7 @@ x, y = vislogprob.logprob(data['Bi (PPM)'])
 
 fig = px.scatter(x=x[::-1]*100, y=y, title='Bi (PPM) x Cumulative Probability', log_x=True, log_y=True, labels={'x':'Probability (%)', 'y': 'Bi (ppm)'})
 
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ]),
 
-    ])
 
 app.layout = html.Div(children=[
     html.H1(children='Geochemical Prospection', style={'textAlign': 'left', 'color': '#054b66'}),
@@ -41,18 +31,26 @@ app.layout = html.Div(children=[
     html.Div([
         html.Div([
             html.Div([
-                generate_table(data, max_rows=20)
-            ], className='four columns'),
+                dash_table.DataTable(
+                    id='table',
+                    columns=[{"name": i, "id": i} for i in data.columns],
+                    data=data.to_dict('records'),
+                    style_table={'overflowX': 'scroll',
+                                 'overflowY': 'scroll',
+                                 'height':'380px'}
+                )
+            ], className='five columns'),
 
             html.Div([
                 dcc.Graph(
                     id='example-graph',
                     figure=fig,
-                    style={'color': '#054b66'}, className='eight columns'
+                    style={'color': '#054b66'}, className='seven columns'
             )])
         ])
     ], className='row'
-    )
+    ),
+    html.Hr()
 ])
 
 if __name__ == '__main__':
