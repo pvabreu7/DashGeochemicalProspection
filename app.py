f9e7bb3f1fcf2825424cca79afe1a59475018f2d
Pedro <pvabreu7@gmail.com>
2020-04-15 19:20:09 -0300

    added tabs for df and freq table

--- app.py
+++ app.py
@@ -13,28 +13,9 @@ import numpy as np
 # Style:
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-layout_table = dict(
-    autosize=True,
-    height=500,
-    font=dict(color="#191A1A"),
-    titlefont=dict(color="#191A1A", size='14'),
-    margin=dict(
-        l=35,
-        r=35,
-        b=35,
-        t=45
-    ),
-    hovermode="closest",
-    plot_bgcolor='#fffcfc',
-    paper_bgcolor='#fffcfc',
-    legend=dict(font=dict(size=10), orientation='h'),
-)
-layout_table['font-size'] = '12'
-layout_table['margin-top'] = '20'
 # Load data:
 data = pd.read_csv('C:/Users/Pedro/PycharmProjects/Geochemical-Prospection/DashGeochemicalProspection/Data/data_asbi.csv')
 x, y = vislogprob.logprob(data['Bi (PPM)'])
-
 fig = px.scatter(x=x[::-1]*100, y=y, title='Bi (PPM) x Cumulative Probability', log_x=True, log_y=True, labels={'x':'Probability (%)', 'y': 'Bi (ppm)'})
 
 
@@ -57,12 +38,18 @@ app.layout = html.Div(children=[
                     html.Div([dcc.Upload(html.Button('Upload your data', style={'width':'100%'}))]),
 
                     html.H4('1.2 Select Geochemical Element:', style={'textAlign': 'center', 'font-size':'20px'}),
-                    dcc.Dropdown(options=[{"label": i, "value": i} for i in data.columns], id='select-element', style={'margin-bottom':'20px'})
+                    dcc.Dropdown(options=[{"label": i, "value": i} for i in data.columns], id='select-element', value='Bi (PPM)',style={'margin-bottom':'20px'})
 
 
 
                 ], className='row'),
-
+                    html.Div([
+                        dcc.Tabs(id='tabs', value='Data Table', children=[
+                            dcc.Tab(children=[
+                                dash_table.DataTable(columns=[{"name": i, "id": i} for i in data.columns], data=data.to_dict('records'))
+                            ]),
+                            dcc.Tab(label='Tab two', value='tab-2')
+                        ]),
                     dash_table.DataTable(
                         id='table',
                         columns=[{"name": i, "id": i} for i in data.columns],
@@ -70,8 +57,8 @@ app.layout = html.Div(children=[
                         style_table={'overflowX': 'scroll',
                                      'overflowY': 'scroll',
                                      'height':'380px'}
-                    )
-                ], className='five columns', style={'border-style':'solid','border-width':'thin', 'padding':'5px', 'border-radius':'8px'})]),
+                    )])
+                ], className='four columns', style={'border-style':'solid','border-width':'thin', 'padding':'5px', 'border-radius':'8px', 'height':'auto'})]),
 
         html.Div([   # Classe Caixa
             html.Div([   # classe 7 colunas
@@ -79,10 +66,9 @@ app.layout = html.Div(children=[
 
                 dcc.Graph(
                     id='prob-graph',
-                    figure=fig,
                     style={'color': '#054b66'}
                 )
-            ], className='seven columns', style={'border-style':'solid','border-width':'thin', 'margin': '20px', 'border-radius':'8px', 'height':'550px'})])
+            ], className='eight columns', style={'border-style':'solid','border-width':'thin', 'margin': '20px', 'border-radius':'8px', 'height':'550px'})])
 
     ], className='row'),
 
@@ -91,7 +77,7 @@ app.layout = html.Div(children=[
 
 @app.callback(
     Output('prob-graph', 'figure'),        # A saída é a figura do gráfico
-    [Input('select-element', 'value')])             # Entrada: valor do slider
+    [Input('select-element', 'value')])             # Entrada: valor da tabela,
 def update_graph(element_column):
     x, y = vislogprob.logprob(data[element_column])
 
@@ -115,6 +101,7 @@ def update_graph(element_column):
                 'title': str(element_column),
                 'type': 'log'
             },
+            title= str(element_column)+' x Cumulative Probability',
             margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
             hovermode='closest'
         )
